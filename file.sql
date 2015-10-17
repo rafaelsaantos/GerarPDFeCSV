359677142c30f7be8ea986a70fb86a213185900e
Rafael Ferreira dos Santos <raafael.saantos@gmail.com>
2015-10-17 00:17:01 -0300

    Commit inicial

---
+++ file.sql
@@ -0,0 +1,45 @@
+# MySQL-Front Dump 2.5
+#
+# Host: localhost   Database: imasters
+# --------------------------------------------------------
+# Server version 4.0.17-nt
+
+
+#
+# Table structure for table 'colunistas'
+#
+
+CREATE TABLE colunistas (ID int(3) NOT NULL auto_increment,
+  NOME varchar(60) NOT NULL default '', ASSUNTO varchar(60) NOT NULL default '', PRIMARY KEY  (ID)) TYPE=MyISAM;
+
+
+
+#
+# Dumping data for table 'colunistas'
+#
+
+INSERT INTO colunistas VALUES("1", "Júlio César Martini", "PHP");
+INSERT INTO colunistas VALUES("2", "Flash", "Maxwell Dayvson");
+INSERT INTO colunistas VALUES("3", "Sql Server", "Mauro Pichiliani");
+INSERT INTO colunistas VALUES("4", "Photoshop", "Fábio Lody");
+INSERT INTO colunistas VALUES("5", "Mobile", "Cristiano Trindade");
+INSERT INTO colunistas VALUES("6", "Corel", "Alex Falcão");
+INSERT INTO colunistas VALUES("7", "3ds Max", "Guilherme Pinheiro");
+INSERT INTO colunistas VALUES("8", "B. Intelligence", "Michel Souza");
+INSERT INTO colunistas VALUES("9", "JavaScript", "Thiago Prado");
+INSERT INTO colunistas VALUES("10", "JDeveloper", "Eduardo Santana");
+INSERT INTO colunistas VALUES("11", "Desenho Vetorial", "Erika Pessanha");
+INSERT INTO colunistas VALUES("12", "Dreamweaver", "Marcelo Oliveira");
+INSERT INTO colunistas VALUES("13", "Segurança", "Alexandre Freire");
+INSERT INTO colunistas VALUES("14", "Fireworks", "Leonardo César");
+INSERT INTO colunistas VALUES("15", "Cisco Systems", "Thiago Dias");
+INSERT INTO colunistas VALUES("16", "Certificações", "Paulo R. Cardoso");
+INSERT INTO colunistas VALUES("17", "Oracle", "Rodrigo Almeida");
+INSERT INTO colunistas VALUES("18", "CSS", "Mauricio Samy");
+INSERT INTO colunistas VALUES("19", "Visual FoxPro", "Nilton Paulino");
+INSERT INTO colunistas VALUES("20", "Delphi", "Daniel Nascimento");
+INSERT INTO colunistas VALUES("21", "Java", "Almedson Ferreira");
+INSERT INTO colunistas VALUES("22", "Redes", "Renato Amadeu");
+INSERT INTO colunistas VALUES("23", "Zope", "Daniel Schmitz");
+INSERT INTO colunistas VALUES("24", "ColdFusion", "Wender Lima");
+INSERT INTO colunistas VALUES("25", "C#", "Caio Azevedo");
